cc232b5487401dadcbfd00ac724029b214c261eb
Richard Osei adu <80524221+sirkofiknight@users.noreply.github.com>
2026-06-30 23:44:38 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'arduino-lab-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './arduino_lab_explorer.html',
+  './manifest.json',
+  './icon.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached response if found, else fetch from network
+        return response || fetch(event.request).then((fetchResponse) => {
+          // Optional: cache new responses dynamically here
+          return fetchResponse;
+        });
+      })
+  );
+});
